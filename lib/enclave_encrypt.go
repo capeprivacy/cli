ded77b8f3d7af526a42c28e24c71617a693f94a0
Benjamin DeCoste <bendecoste@gmail.com>
2022-05-12 09:38:48 -0300

    expose a library directory
    this exposes the local encrypt so that enclave-server can temporarily
    use it

---
+++ lib/enclave_encrypt.go
@@ -0,0 +1,31 @@
+package lib
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/capeprivacy/cli/attest"
+	"github.com/google/tink/go/hybrid"
+	"github.com/google/tink/go/keyset"
+)
+
+func LocalEncrypt(doc attest.AttestationDoc, plaintext []byte) ([]byte, error) {
+	buf := bytes.NewBuffer(doc.PublicKey)
+	reader := keyset.NewBinaryReader(buf)
+	khPub, err := keyset.ReadWithNoSecrets(reader)
+	if err != nil {
+		return nil, fmt.Errorf("read pubic key %s", err)
+	}
+
+	encrypt, err := hybrid.NewHybridEncrypt(khPub)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext, err := encrypt.Encrypt(plaintext, nil)
+	if err != nil {
+		return nil, fmt.Errorf("unable to encrypt %s", err)
+	}
+
+	return ciphertext, nil
+}
