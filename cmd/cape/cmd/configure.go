e5b31050ca668c4c92362b5d04803218539542c7
Rahul Ramesh <rahul@capeprivacy.com>
2022-07-20 14:08:55 -0400

    added configure option to cli

---
+++ cmd/cape/cmd/configure.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var configureCmd = &cobra.Command{
+	Use:   "configure key value",
+	Short: "Configure and save persistent parameters for Cape",
+	Long: "Configure and save persistent parameters for Cape\n" +
+		"Use the configure option to set global parameters and save to a json file for later use.\n" +
+		"Default is $HOME/.config/cape/presets.json",
+	RunE: Configure,
+}
+
+func init() {
+	rootCmd.AddCommand(configureCmd)
+}
+
+func Configure(cmd *cobra.Command, args []string) error {
+	if len(args) != 2 {
+		if err := cmd.Usage(); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	configFile := viper.ConfigFileUsed()
+	return UpdateConfigFileJson(configFile, args[0], args[1])
+}
+
+func UpdateConfigFileJson(jsonFile string, key string, value string) error {
+	// Read json buffer from jsonFile
+	byteValue, err := ioutil.ReadFile(jsonFile)
+	if err != nil {
+		return err
+	}
+
+	var config map[string]interface{}
+	err = json.Unmarshal(byteValue, &config)
+	if err != nil {
+		return err
+	}
+
+	config[key] = value
+
+	// Convert golang object back to byte
+	byteValue, err = json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	// Write back to file
+	err = ioutil.WriteFile(jsonFile, byteValue, 0644)
+	return err
+}
