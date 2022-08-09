62aefb7014747d289c3c420c5ac3f92b420e4f61
Rahul Ramesh <rahul@capeprivacy.com>
2022-08-09 15:57:15 -0400

    lint

--- cmd/cape/cmd/root.go
+++ cmd/cape/cmd/root.go
@@ -54,8 +54,7 @@ var rootCmd = &cobra.Command{
 // ExecuteCLI adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func ExecuteCLI() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if rootCmd.Execute() != nil {
 		fmt.Println("Command failed with error.")
 		os.Exit(1)
 	}
