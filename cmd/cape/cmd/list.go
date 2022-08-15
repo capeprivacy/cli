169216501da7c9af4f854b8c1d12b7b2f0872445
Jenny <jenny@capeprivacy.com>
2022-08-15 14:13:53 -0400

    cleanup

--- cmd/cape/cmd/list.go
+++ cmd/cape/cmd/list.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/capeprivacy/go-kit/id"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
+
+	"github.com/capeprivacy/go-kit/id"
 )
 
 type DeploymentName struct {
