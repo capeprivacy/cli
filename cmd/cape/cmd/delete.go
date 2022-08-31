e23438ab5ba2b2b1ba9ad5e48675560adb2f28b9
Rahul Ramesh <rahul@capeprivacy.com>
2022-08-31 14:40:23 -0400

    removed runtime from request path

--- cmd/cape/cmd/delete.go
+++ cmd/cape/cmd/delete.go
@@ -1,16 +1,13 @@
 package cmd
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
+	"net/http"
 
-	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
-	"github.com/capeprivacy/cli/crypto"
 	"github.com/capeprivacy/cli/entities"
-	"github.com/capeprivacy/cli/protocol"
 )
 
 // deleteCmd represents the run command
@@ -66,45 +63,18 @@ func delete(cmd *cobra.Command, args []string) error {
 func doDelete(url string, functionID string, insecure bool, auth entities.FunctionAuth) error { //nolint:gocognit
 	endpoint := fmt.Sprintf("%s/v1/delete/%s", url, functionID)
 
-	authProtocolType := "cape.runtime"
-	c, res, err := websocketDial(endpoint, insecure, authProtocolType, auth.Token)
-	if err != nil {
-		log.Error("error dialing websocket: ", err)
-		// This check is necessary because we don't necessarily return an http response from sentinel.
-		// Http error code and message is returned if network routing fails.
-		if res != nil {
-			var e ErrorMsg
-			if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
-				return err
-			}
-			res.Body.Close()
-			return fmt.Errorf("error code: %d, reason: %s", res.StatusCode, e.Error)
-		}
-		return err
-	}
-	defer c.Close()
-
-	nonce, err := crypto.GetNonce()
+	req, err := http.NewRequest("DELETE", endpoint, nil)
 	if err != nil {
 		return err
 	}
 
-	p := protocol.Protocol{Websocket: c}
-
-	req := entities.StartRequest{Nonce: []byte(nonce), AuthToken: auth.Token}
-	log.Debug("\n> Sending Nonce and Auth Token")
-	err = p.WriteStart(req)
-	if err != nil {
-		return errors.Wrap(err, "error writing delete request")
-	}
-
-	resData, err := p.ReadDeleteResults()
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
-	log.Debugf("< Received Delete Results.")
-	if !resData.Done {
-		return fmt.Errorf("delete failed with error")
+
+	if res.StatusCode != 200 {
+		return errors.New("delete failed")
 	}
 
 	return nil
