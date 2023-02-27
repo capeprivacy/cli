f6ba940e8725a8e4614d5fd05c42346e70b90061
Eric <eric@capeprivacy.com>
2023-02-27 15:09:49 -0500

    adding test for parseinput

--- cmd/cape/cmd/encrypt_test.go
+++ cmd/cape/cmd/encrypt_test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/capeprivacy/cli/sdk"
@@ -24,15 +25,32 @@ func TestEncrypt(t *testing.T) {
 	}
 }
 
-func TestEncryptEmpty(t *testing.T) {
-	capeEncrypt = func(message, username string, options ...sdk.Option) (string, error) {
-		return "cape:secret", nil
-	}
-
-	cmd, _, _ := getCmd()
-	cmd.SetArgs([]string{"encrypt", "", "--username", "bendecoste"})
-	wantErr := UserError{Msg: "unable to encrypt", Err: errors.New("input is empty")}
-	if err := cmd.Execute(); err.Error() != wantErr.Error() {
-		t.Fatalf("expected error: %s", wantErr.Error())
+func TestEncryptBadInput(t *testing.T) {
+	for _, tt := range []struct {
+		name      string
+		args      []string
+		wantError error
+	}{
+		{
+			name:      "empty input",
+			args:      []string{"encrypt", "", "--username", "bendecoste"},
+			wantError: UserError{Msg: "unable to encrypt", Err: errors.New("input is empty")},
+		},
+		{
+			name:      "too many inputs",
+			args:      []string{"encrypt", "", "", "--username", "bendecoste"},
+			wantError: UserError{Msg: "you must pass in only one input data (stdin, string or filename)", Err: fmt.Errorf("invalid number of input arguments")},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			capeEncrypt = func(message, username string, options ...sdk.Option) (string, error) {
+				return "cape:secret", nil
+			}
+			cmd, _, _ := getCmd()
+			cmd.SetArgs(tt.args)
+			if err := cmd.Execute(); err.Error() != tt.wantError.Error() {
+				t.Fatalf("expected error: %s, got: %s", tt.wantError.Error(), err.Error())
+			}
+		})
 	}
 }
